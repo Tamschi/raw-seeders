73e72da54275101a5c9f1c5aa54a66da4ff777ac
Tamme Schichler <tamme@schichler.dev>
2020-08-14 10:28:36 +0200

    Added IEEE754(ReprSeeder)

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,10 @@ use {
         ser,
     },
     serde_seeded::Seeder,
+    wyz::Pipe as _,
 };
 
+#[derive(Debug, Clone, Copy, PartialEq, Ord, PartialOrd, Eq)]
 pub struct Literal<'a>(pub &'a [u8]);
 impl<'a: 's, 's> Seeder<'s, ()> for Literal<'a> {
     type Seed = Self;
@@ -64,6 +66,7 @@ impl<'a, 'de> de::DeserializeSeed<'de> for Literal<'a> {
     }
 }
 
+#[derive(Debug, Copy, Clone, Default)]
 pub struct LittleEndian;
 impl<'s, T: 's + ByteOrdered> Seeder<'s, T> for LittleEndian {
     type Seed = LittleEndianSeed<T>;
@@ -76,6 +79,7 @@ impl<'s, T: 's + ByteOrdered> Seeder<'s, T> for LittleEndian {
     }
 }
 
+#[derive(Debug, Copy, Clone, Default)]
 pub struct LittleEndianSeed<T>(PhantomData<T>);
 impl<'de, T: ByteOrdered> de::DeserializeSeed<'de> for LittleEndianSeed<T> {
     type Value = T;
@@ -87,6 +91,7 @@ impl<'de, T: ByteOrdered> de::DeserializeSeed<'de> for LittleEndianSeed<T> {
     }
 }
 
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
 pub struct LittleEndianSeeded<'a, T>(&'a T);
 impl<'a, T: ByteOrdered> ser::Serialize for LittleEndianSeeded<'a, T> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
@@ -111,11 +116,72 @@ impl ByteOrdered for u32 {
     }
 }
 
-impl ByteOrdered for f32 {
-    fn deserialize_le<'de, D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        Ok(Self::from_le_bytes(PhantomData.deserialize(deserializer)?))
+#[derive(Debug, Copy, Clone, Default)]
+pub struct IEEE754<ReprSeeder>(pub ReprSeeder);
+impl<'s, T: 's + IEEE754able, ReprSeeder: Clone + for<'repr> Seeder<'repr, T::Repr> + 's>
+    Seeder<'s, T> for IEEE754<ReprSeeder>
+{
+    type Seed = IEEE754Seed<T, ReprSeeder>;
+    type Seeded = IEEE754Seeded<'s, T, ReprSeeder>;
+    fn seed(self) -> Self::Seed {
+        IEEE754Seed(self.0, PhantomData)
     }
-    fn serialize_le<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        serializer.serialize_bytes(&self.to_le_bytes())
+    fn seeded(self, value: &'s T) -> Self::Seeded {
+        IEEE754Seeded(value, self.0)
+    }
+}
+
+#[derive(Debug, Copy, Clone, Default)]
+pub struct IEEE754Seed<T, ReprSeeder>(ReprSeeder, PhantomData<T>);
+impl<'de, T: IEEE754able, ReprSeeder: for<'d> Seeder<'d, T::Repr>> de::DeserializeSeed<'de>
+    for IEEE754Seed<T, ReprSeeder>
+{
+    type Value = T;
+    fn deserialize<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        self.0.seed().deserialize(deserializer).map(T::from)
+    }
+}
+
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct IEEE754Seeded<'a, T, ReprSeeder>(&'a T, ReprSeeder);
+impl<'a, T: IEEE754able, ReprSeeder: Clone + for<'b> Seeder<'b, T::Repr>> ser::Serialize
+    for IEEE754Seeded<'a, T, ReprSeeder>
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.0
+            .to()
+            .pipe(|repr| self.1.clone().seeded(&repr).serialize(serializer))
+    }
+}
+
+pub trait IEEE754able {
+    type Repr;
+    fn from(repr: Self::Repr) -> Self;
+    fn to(&self) -> Self::Repr;
+}
+
+impl IEEE754able for f32 {
+    type Repr = u32;
+    fn from(repr: Self::Repr) -> Self {
+        f32::from_bits(repr)
+    }
+    fn to(&self) -> Self::Repr {
+        self.to_bits()
+    }
+}
+
+impl IEEE754able for f64 {
+    type Repr = u64;
+    fn from(repr: Self::Repr) -> Self {
+        f64::from_bits(repr)
+    }
+    fn to(&self) -> Self::Repr {
+        self.to_bits()
     }
 }
